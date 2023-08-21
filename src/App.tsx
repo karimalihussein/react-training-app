5271078d1c8e8bfa73cc5ba3fa0fcb2415e4aae7
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-21 09:02:09 +0300

    work with Vanilla css

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,36 @@ import { useState } from "react";
 import Message from "./Message";
 import  Alert  from "./components/Alert";
 import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
+import ListGroup from "./components/ListGrpup";
 
-
-// function App() {
-//   let items = [
-//     "New York",
-//     "Los Angeles",
-//     "Chicago",
-//     "Houston",
-//     "Philadelphia",
-//     "Phoenix",
-//     "San Antonio",
-//     "San Diego",
-//   ];
-//   const handleSelectItem = (item: string) => {
-//     console.log(item);
-//   };
-//   return <div> <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}  /> </div>
-// }
+function App() {
+  let items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Philadelphia",
+    "Phoenix",
+    "San Antonio",
+    "San Diego",
+  ];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return <div> <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}  /> </div>
+}
 
 
 // function App() {
 //   return (<div> <Alert > <h1>hh</h1> </Alert> </div>);
 // }
 
-function App() {
-  const [alertVisible, setAlertVisiblity] = useState(false);
-    return ( <div> 
-      { alertVisible && <Alert onClose={() => setAlertVisiblity(false)} >Welcome To My Alert</Alert> }
-      <Button onClick={() => setAlertVisiblity(true) } color="dark" >Karm</Button> 
-      </div> );
-}
+// function App() {
+//   const [alertVisible, setAlertVisiblity] = useState(false);
+//     return ( <div> 
+//       { alertVisible && <Alert onClose={() => setAlertVisiblity(false)} >Welcome To My Alert</Alert> }
+//       <Button onClick={() => setAlertVisiblity(true) } color="dark" >Karm</Button> 
+//       </div> );
+// }
 
 export default App;
