35adf2f104a972707104cb5f7225a9fd4682845f
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-21 06:21:05 +0300

    managing State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 // import { Fragment } from "react";
 function listGroup() {
-  const items = [
+  let items = [
     "New York",
     "Los Angeles",
     "Chicago",
@@ -10,13 +12,37 @@ function listGroup() {
     "San Antonio",
     "San Diego",
   ];
+  // items = [];
+  const getMessage = () => {
+    return items.length === 0 ? "There are no items to display" : null;
+  };
+
+  const handleOnClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>
+  ) => {
+    console.log(event);
+  };
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List Group</h1>
+      {items.length === 0 && <p>There are no items to display</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} >{item}</li>
+        {items.map((item, index) => (
+          <li
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              console.log(index);
+            }}
+            className={`list-group-item ${
+              index === selectedIndex ? "active" : ""
+            }`}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
