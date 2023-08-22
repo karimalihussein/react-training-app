80c476db8a9c4c8b227436cf107d39995e672af6
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-22 22:04:57 +0300

    work on showing a loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,26 @@ interface User {
 function App() {
   const [data, setData] = useState<User[]>([]);
   const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
   // get -> promise -> then -> catch
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     axios.get<User[]>("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
-    .then((res) => setData(res.data))
+    .then((res) => {setData(res.data); setLoading(false); })
     .catch((error) => {
         if(error instanceof CanceledError) return;
         if(error instanceof AxiosError) setError(error.message);
-    } );
+        setLoading(false);
+    });
     return () => controller.abort();
 }, []);
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
+      {loading &&  <div className="spinner-border">
+        <span className="visually-hidden">Loading...</span>
+      </div> }
       <h1>Users</h1>
       <ul>
         {data.map((user) => (
