cfe666a4b126393d73c175d6e85094e35ea66891
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-22 21:48:44 +0300

    work on canceling fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
-
 
 function App() {
-    const [data, setData] = useState<User[]>([]);
-    const [error, setError] = useState<string>('');
-    useEffect(() => {
-        axios.get<User[]>("https://jsonplaceholder.typicode.com/users").then((res) => setData(res.data)).catch(
-            (err) => setError(err.message)
-        );
-    }, []);
-    return (
-        <div>
-            {error && <p className="text-danger">{error}</p>}
-            <h1>Users</h1>
-            <ul>
-                {data.map((user) => (
-                    <li key={user.id}>{user.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  const [data, setData] = useState<User[]>([]);
+  const [error, setError] = useState<string>("");
+  // get -> promise -> then -> catch
+  useEffect(() => {
+    const controller = new AbortController();
+    axios.get<User[]>("https://jsonplaceholder.typicode.com/users", { signal: controller.signal })
+    .then((res) => setData(res.data))
+    .catch((error) => {
+        if(error instanceof CanceledError) return;
+        if(error instanceof AxiosError) setError(error.message);
+    } );
+    return () => controller.abort();
+}, []);
+  return (
+    <div>
+      {error && <p className="text-danger">{error}</p>}
+      <h1>Users</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 
 export default App;
