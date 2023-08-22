9ebda790e43fc1c09e6ba48456fdde1b9d7dcf87
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-22 06:52:24 +0300

    clean app

--- src/components/tracker/components/ExpensesFilter.tsx
+++ src/components/tracker/components/ExpensesFilter.tsx
@@ -8,11 +8,11 @@ interface Props {
 const ExpensesFilter = ({ onSelectCategory}: Props) => {
   return (
     <select className='form-select' onChange={(event) => onSelectCategory(event.target.value)}>
-        <option>All Categories</option>
+        <option value="">All Categories</option>
         <option value='Groceries'>Groceries</option>
         <option value='Utilities'>Utilities</option>
         <option value='Entertainment'>Entertainment</option>
-        <option value='Transportation'>Education</option>
+        <option value='Education'>Education</option>
     </select>
   )
 }
