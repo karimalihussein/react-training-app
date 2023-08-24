725dddb439ea2f117029d0e8da5cec29cda2ad2b
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-24 11:35:26 +0300

    handle the games with no image to add DefaultImage

--- src/services/ImageUrl.ts
+++ src/services/ImageUrl.ts
@@ -1,5 +1,7 @@
+import DefaultImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (image: string, width: number, height: number) => {
-    if(!image) return '';
+    if(!image) return DefaultImage;
     const target = 'media/';
     const index = image.indexOf(target) + target.length;
     return image.slice(0, index) + `crop/${width}/${height}/` + image.slice(index);
