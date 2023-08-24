723a707c495b03475ab4c94643df4ee9aed19635
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-24 15:10:11 +0300

    Refactor game grid

--- src/components/games/GameGrid.tsx
+++ src/components/games/GameGrid.tsx
@@ -1,5 +1,5 @@
 import useGames from "../../hooks/useGames";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
@@ -15,14 +15,12 @@ interface IGameGridProps {
 export const GameGrid = ({ gameQuery }: IGameGridProps) => {
   const { data, loading, error } = useGames(gameQuery);
   const skeletions = [1, 2, 3, 4, 5, 4];
+  if(error) return <Text> {error} </Text>
   return (
-    <div>
-      {error && <div>Something went wrong</div>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={'10px'} spacing={6}>
         {loading  && skeletions.map((skeletion) => ( <GameCardContainer key={skeletion}> <GameCardSkeleton  /> </GameCardContainer> ))}
         {!loading && !error && data.length === 0 && <div>No games found</div>}
         {data.map((game) => ( <GameCardContainer key={game.id}> <GameCard  game={game} /> </GameCardContainer> ))}
       </SimpleGrid>
-    </div>
   );
 };
