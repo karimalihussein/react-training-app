f24f7515e89682baace45fd49396f8c0331bce57
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-24 15:08:25 +0300

    update them colors

--- src/components/games/GameCard.tsx
+++ src/components/games/GameCard.tsx
@@ -14,13 +14,13 @@ const GameCard = ({ game }: Props) => {
     <Card >
         <Image src={getCroppedImageUrl(game.background_image, 600, 400)} alt={game.name} />
         <CardBody>
-            <Heading fontSize={'1xl'}>
-              {game.name.length > 20 ? game.name.substring(0, 20) + '...' : game.name}
-            </Heading>
-            <HStack justifyContent={'space-between'}>
+            <HStack justifyContent={'space-between'} marginBottom={3}>
               <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
               <CriticScore score={game.metacritic} />
             </HStack>
+            <Heading fontSize={'2xl'}>
+              {game.name.length > 20 ? game.name.substring(0, 15) + '...' : game.name}
+            </Heading>
         </CardBody>
     </Card>
   )
--- src/components/games/GameGrid.tsx
+++ src/components/games/GameGrid.tsx
@@ -14,11 +14,11 @@ interface IGameGridProps {
 
 export const GameGrid = ({ gameQuery }: IGameGridProps) => {
   const { data, loading, error } = useGames(gameQuery);
-  const skeletions = [1, 2, 3, 4, 5, 6];
+  const skeletions = [1, 2, 3, 4, 5, 4];
   return (
     <div>
       {error && <div>Something went wrong</div>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={'10px'} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={'10px'} spacing={6}>
         {loading  && skeletions.map((skeletion) => ( <GameCardContainer key={skeletion}> <GameCardSkeleton  /> </GameCardContainer> ))}
         {!loading && !error && data.length === 0 && <div>No games found</div>}
         {data.map((game) => ( <GameCardContainer key={game.id}> <GameCard  game={game} /> </GameCardContainer> ))}
