7111d51c3637ff2267a87979b1173d311dcdaac4
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-25 07:33:26 +0300

    last update on game-hub

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import theme from './theme'
-import  './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from './theme';
+import  './index.css';
+
+const queryClient = new QueryClient();
+
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface ITodo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const TodoList = () => {
+    const fetchTodos = () => axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos').then((res) => res.data);
+    const { data, error } = useQuery<ITodo[], Error >({queryKey: ['todos'], queryFn: fetchTodos});
+
+    if(error) return <p>{ error.message }</p>
+
+    return (
+        <ul className="list-group">
+            {data?.map((todo) => (
+                <li className="list-group-item" key={todo.id}>
+                    {todo.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
