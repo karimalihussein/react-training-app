78c3eee56e0b88d752156b8132c5bfe3b10a094c
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-25 07:49:30 +0300

    create a custom query hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,18 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import useTodos from "./hooks/useTodos";
 
-interface ITodo {
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
-}
+
 
 const TodoList = () => {
-    const fetchTodos = () => axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos').then((res) => res.data);
-    const { data, error, isLoading } = useQuery<ITodo[], Error >({queryKey: ['todos'], queryFn: fetchTodos});
-
+    const { data, error, isLoading } = useTodos();
     if(isLoading) return <p>Loading...</p>;
     if(error) return <p>{ error.message }</p>;
 
---
+++ src/react-query/hooks/useTodos.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+export interface ITodo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const useTodos = () => {
+    const fetchTodos = () => axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos').then((res) => res.data);
+    return useQuery<ITodo[], Error >({queryKey: ['todos'], queryFn: fetchTodos});
+};
+
+export default useTodos;
