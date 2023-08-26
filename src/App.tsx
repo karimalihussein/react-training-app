0e0ca3c4685d1de8250f143b1619ce38cfd1cb71
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-08-27 02:49:49 +0300

    update app comp

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import GenreList from "./components/games/GenreList";
 import PlatFormSelector from "./components/games/PlatFormSelector";
 import SortSelector from "./components/games/SortSelector";
 import GameHeading from "./components/games/GameHeading";
-import TaskList from "./state-management/TaskList";
 
 
 export interface IGameQuery {
@@ -21,7 +20,27 @@ function App() {
   const templateAreas = { base: `"nva" "main"`, lg: `"nva nva" "aside main"`};
   const templateColumns = { base: "1fr", lg: "300px 1fr"}
   return (
-    <TaskList />
+    <Grid templateAreas={templateAreas} templateColumns={templateColumns}>
+      <GridItem gridArea="nva"> <NavBar onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText})} /> </GridItem>
+
+      <Show above="lg">
+          <GridItem gridArea="aside" paddingX={5}>
+             <GenreList selectedGenreId={gameQuery.genreId} onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genreId: genre.id})} />
+          </GridItem>
+      </Show>
+
+      <GridItem gridArea="main">
+          <GameHeading gameQuery={gameQuery} />
+         <Flex  paddingLeft={2} marginBottom={5}>
+          <Box marginRight={5}>
+               <PlatFormSelector selectedPlatformId={gameQuery.platformId} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platformId: platform.id })} />
+            </Box>
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSort={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
+         </Flex>
+         <GameGrid gameQuery={gameQuery} />
+      </GridItem>
+
+    </Grid>
   );
 }
 
